84a5ffcfb9eb490b542e820f21829b3f9b638020
Rafael Santos <rafaelnsantos1@gmail.com>
2025-02-19 21:25:23 -0300

    add carousel test

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {RouterLink} from "@angular/router";
 import {MatButton} from "@angular/material/button";
@@ -13,15 +13,48 @@ import {LoadingService} from "../../../shared/services/loading/loading.service";
     MatSidenavModule,
     RouterLink,
     MatButton,
-    MatProgressBar
+    MatProgressBar,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  slide = 0;
+
+  getSlide() {
+    return this.slides[this.slide]
+  }
+
+  getNext() {
+    if (this.slide > this.slides.length - 2) {
+      this.slide = 0;
+    } else {
+      this.slide++;
+    }
+  }
+
+  getPrev() {
+    if (this.slide < 1) {
+      this.slide = this.slides.length - 1;
+    } else {
+      this.slide--;
+    }
+  }
 
   authService = inject(AuthService)
   loadingService = inject(LoadingService)
+
+  slides = ['./assets/img/angular.jpg', './assets/img/react.jpg', './assets/img/vue.jpg']
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.getNext()
+    }, 4000)
+  }
+
+  onItemChange($event: any): void {
+    console.log('Carousel onItemChange', $event);
+  }
 
   handleLogin() {
     this.authService.login$('').subscribe()
